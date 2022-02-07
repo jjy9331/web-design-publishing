9d5813c0a4422dc3001b4139ea74e2fba395c3dc
Jung jiyong <jjy9331@naver.com>
2022-02-07 16:21:57 +0900

    220207 js funtion practice add

--- practice/javascript_part1/05_함수/js/함수2.js
+++ practice/javascript_part1/05_함수/js/함수2.js
@@ -5,35 +5,3 @@ function AA() { //AA라는 기능
 
 AA();//AA함수를 호출(AA기능을 실행)
 
-function BB(b,c,d) { //BB라는 기능 
-
-    let target = document.getElementById(b)
-
-    target.style.transition="1s";
-    target.style.borderRadius=c;
-    target.style.transform="rotate(360deg) scale(0.5,0.5)";
-    target.style.background="hotpink";
-    target.style.backgroundColor=d;
-}
-
-
-function CC() { //CC라는 기능 
-
-let target = document.getElementById("nemo")
-
-target.style.transition="1s"
-target.style.borderRadius="20px"
-target.style.transform="scale(0.5,0.5)";
-target.style.background="hotpink";
-}
-
-function roScale(a,b,c) { 
-
-let target = document.getElementById(a);
-
-target.style.transition="1s"
-target.style.borderRadius="20px"
-target.style.transform="scale(0.5,0.5)";
-target.style.backgroundColor=b;
-target.style.background=c;
-}
