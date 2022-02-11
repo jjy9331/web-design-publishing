6a7145edf31af759c8be23f3067ce6ec63ee1684
Jung jiyong <jjy9331@naver.com>
2022-02-11 12:13:21 +0900

    220211 JS navigation for loop update

--- practice/javascript_part2/8_navigation/navigation.js
+++ practice/javascript_part2/8_navigation/navigation.js
@@ -2,39 +2,43 @@
 
 let main =  document.getElementsByClassName("main");
 
-let sub =  document.getElementsByClassName("sub");
+// let sub =  document.getElementsByClassName("sub");
 
-// for(i=0; i<3; i++){
-    // sub[i].style.display="none" 
-    sub[0].style.display="none" 
-    sub[1].style.display="none" 
-    sub[2].style.display="none" 
-// }
+for(i=0; i<3; i++){
+    main[i].children[0].style.display="none"; 
+}
 
-// sub[0].style.display="none"
-// sub[1].style.display="none"
-// sub[2].style.display="none"
+for(i=0; i<3; i++){
+    main[i].addEventListener("mouseover",function(){
+        this.children[0].style.display="block";
+    });
+    main[i].addEventListener("mouseout",function(){
+        this.children[0].style.display="none";
+    });
+}
 
-main[0].addEventListener("mouseover",function(){
-    sub[0].style.display="block";
-});
 
-main[0].addEventListener("mouseout",function(){
-    sub[0].style.display="none";
-});
 
-main[1].addEventListener("mouseover",function(){
-    sub[1].style.display="block";
-});
+// main[0].addEventListener("mouseover",function(){
+//     this.children[0].style.display="block";
+// });
 
-main[1].addEventListener("mouseout",function(){
-    sub[1].style.display="none";
-});
+// main[0].addEventListener("mouseout",function(){
+//     this.children[0].style.display="none";
+// });
 
-main[2].addEventListener("mouseover",function(){
-    sub[2].style.display="block";
-});
+// main[1].addEventListener("mouseover",function(){
+//     this.children[0].style.display="block";
+// });
 
-main[2].addEventListener("mouseout",function(){
-    sub[2].style.display="none";
-});
+// main[1].addEventListener("mouseout",function(){
+//     this.children[0].style.display="none";
+// });
+
+// main[2].addEventListener("mouseover",function(){
+//     this.children[0].style.display="block";
+// });
+
+// main[2].addEventListener("mouseout",function(){
+//     this.children[0].style.display="none";
+// });
