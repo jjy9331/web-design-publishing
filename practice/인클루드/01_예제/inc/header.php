9421b5b3ca854b36163717cf7c68506164da71e4
Jung jiyong <jjy9331@naver.com>
2022-04-20 10:26:36 +0900

    220420 include php practice

---
+++ practice/인클루드/01_예제/inc/header.php
@@ -0,0 +1,9 @@
+<div id="header">
+        <div class="inner">
+		   <h1><a href="index.php">로고</a></h1>
+		   <ul class="gnb">
+		   	 <li><a href="sub1-1.php">회사소개</a></li>
+		   	 <li><a href="sub2-1.php">홍보</a></li>
+		   </ul>
+		</div>
+  </div>
