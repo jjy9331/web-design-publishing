8a54f1fc54e1ced1b33b4e41fb492491c79009a7
Jung jiyong <jjy9331@naver.com>
2022-04-20 10:55:29 +0900

    220420 include php header update

--- practice/인클루드/01_예제/inc/header.php
+++ practice/인클루드/01_예제/inc/header.php
@@ -1,9 +1,9 @@
 <div id="header">
         <div class="inner">
-		   <h1><a href="index.php">로고</a></h1>
+		   <h1><a href="index.php">logo</a></h1>
 		   <ul class="gnb">
-		   	 <li><a href="sub1-1.php">회사소개</a></li>
-		   	 <li><a href="sub2-1.php">홍보</a></li>
+		   	 <li><a href="sub1-1.php">company</a></li>
+		   	 <li><a href="sub2-1.php">advertisement</a></li>
 		   </ul>
 		</div>
   </div>
