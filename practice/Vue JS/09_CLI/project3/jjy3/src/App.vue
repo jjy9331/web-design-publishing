81dfe65b49a9020cc0df720b309aa0c59bd544fa
Jung jiyong <jjy9331@naver.com>
2022-05-03 11:59:52 +0900

    220503 VUE CLI component practice

--- practice/Vue JS/09_CLI/project3/jjy3/src/App.vue
+++ practice/Vue JS/09_CLI/project3/jjy3/src/App.vue
@@ -7,18 +7,31 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue"
 import Contents from "./components/Contents.vue"
+import Header from "./components/Header.vue"
 import Footer from "./components/Footer.vue"
+
 export default {
-  components:{
-    "Contents":Contents,
-    "Header":Header,
-    "Footer":Footer,
-  }
+    components:{
+        "Contents":Contents,
+        "Header":Header,
+        "Footer":Footer,
+    }
 }
 </script>
 
 <style>
+/* reset */
+html,body { height:100%; }
+* { margin:0; padding:0; }
+ul { list-style:none;}
 
 </style>
+
+
+
+
+
+
+
+
--- practice/Vue JS/09_CLI/project3/jjy3/src/components/Contents.vue
+++ practice/Vue JS/09_CLI/project3/jjy3/src/components/Contents.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="contents">
+    
     <h2>메뉴 카테고리</h2>
+    
     <ul>
-      <li>
-        메인메뉴
-          <ul class="sub">
-            <li>서브메뉴</li>
-            <li>서브메뉴</li>
-            <li>서브메뉴</li>
-          </ul>
-    </li>
+        <li class="main" v-on:click="menuOpen">
+            메인메뉴
+            <ul class="sub">
+                <li>서브메뉴</li>
+                <li>서브메뉴</li>
+                <li>서브메뉴</li>
+            </ul>
+        </li>
     </ul>
-
+    
+    
   </div>
 </template>
 
 <script>
 export default {
-  methods:{
-    AA:function(){
-      document.getElementsByClassName("sub")[0].style.display="block";
-    },
-  }
-
+    methods:{
+        menuOpen:function(){
+            document.getElementsByClassName("sub")[0].style.display="block";
+        },
+    }
 }
 </script>
 
 <style>
-.sub {display:none;}
+.sub { display:none; }
 </style>
+
+
+
+
+
+
+
+
+
+
+
 
