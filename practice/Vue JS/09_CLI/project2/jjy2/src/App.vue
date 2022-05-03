b9e2de8a6e4d07a7bde127faa732b09751990f7c
Jung jiyong <jjy9331@naver.com>
2022-05-03 10:51:57 +0900

    220503 VUE CLI practice component

---
+++ practice/Vue JS/09_CLI/project2/jjy2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="home">
+    <Room></Room>
+  </div>
+</template>
+
+<script>
+import Room from "./components/Room.vue"
+
+export default {
+  data(){
+    return{a:1, b:2}
+  },
+  created:function(){},
+  methods:{
+    AA:function(){},
+    BB:function(){},
+  },
+  components:{
+    "Room":Room,
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ practice/Vue JS/09_CLI/project2/jjy2/src/components/Room.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="room">
+    룸
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#room {border 2px dashed orange}
+</style>
