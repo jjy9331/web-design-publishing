f99e2b69d07a1ea6832923094900f61ddc467da5
Jung jiyong <jjy9331@naver.com>
2022-05-03 11:01:50 +0900

    220503 VUE CLI practice component p1

--- practice/Vue JS/09_CLI/project1/jjy/src/App.vue
+++ practice/Vue JS/09_CLI/project1/jjy/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div id="home">
-    <h1>홈</h1>
-    <p>{{ c }}</p>
-    <button v-on:click="AA">버튼</button>
-    <button v-on:click="BB">버튼</button>
-    <button v-on:click="CC">버튼</button>
-  </div>
+    <div id="home">
+        <h1>홈</h1>
+        <p>{{ c }}</p>
+        <button v-on:click="AA">버튼</button>
+        <button v-on:click="BB">버튼</button>
+        <button v-on:click="CC">버튼</button>
+    </div>
 </template>
 
 <script>
 export default {
-  data:{ a:1, b:2 },
-  created:function(){},//시작했을 때 실행
-  methods:{},
-    AA:function(){
-      alert("안녕하세요");
+    data(){
+        return { a:1, b:2, c:"행복하세요" }
     },
-    BB:function(){
-      alert("반갑습니다");
+    created:function(){//시작했을때 실행
+        //alert(777);
     },
-    CC:function(){
-      alert(this.b);
+    methods:{//클릭 등 이벤트 발생시 실행할 함수
+        AA:function(){
+            alert("안녕하세요");
+        },
+        BB:function(){
+            alert("반갑습니다");
+        },  
+        CC:function(){
+            alert(this.b);
+        },         
     },
 }
 </script>
 
 <style>
-h1 {border:5px dashed orange;}
+h1 { border:5px dashed orange; }
 </style>
+
+
+
+
+
+
+
