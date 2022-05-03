c9be63100abadd8fa98d9d0e4e38fdd56eb16c57
Jung jiyong <jjy9331@naver.com>
2022-05-03 10:21:39 +0900

    220503 VUE CLI test

--- practice/Vue JS/09_CLI/project1/jjy/src/App.vue
+++ practice/Vue JS/09_CLI/project1/jjy/src/App.vue
@@ -1,60 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="home">
+    <h1>홈</h1>
+    <p>{{ c }}</p>
+    <button v-on:click="AA">버튼</button>
+    <button v-on:click="BB">버튼</button>
+    <button v-on:click="CC">버튼</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  data:{ a:1, b:2 },
+  created:function(){},//시작했을 때 실행
+  methods:{},
+    AA:function(){
+      alert("안녕하세요");
+    },
+    BB:function(){
+      alert("반갑습니다");
+    },
+    CC:function(){
+      alert(this.b);
+    },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+h1 {border:5px dashed orange;}
 </style>
