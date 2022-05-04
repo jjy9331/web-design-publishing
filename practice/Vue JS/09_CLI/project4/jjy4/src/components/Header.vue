56bd8fe71c7705c7f0247404bf4a47cd964bc4d7
Jung jiyong <jjy9331@naver.com>
2022-05-04 10:44:55 +0900

    220504 VUE CLI components methods update

--- practice/Vue JS/09_CLI/project4/jjy4/src/components/Header.vue
+++ practice/Vue JS/09_CLI/project4/jjy4/src/components/Header.vue
@@ -1,23 +1,38 @@
 <template>
   <div id="header">
     <h1>로고</h1>
-    <button>홈</button>
-    <button>제품</button>
-    <button>연락처</button>
+    <button v-on:click="AA">홈</button>
+    <button v-on:click="AA">제품</button>
+    <button v-on:click="AA" >연락처</button>
   </div>
 </template>
 
 <script>
-export default {
-  created:function(){
 
-    let num = document.getElementsByClassName("con").length;
+let num;
+
+export default {
+  mounted:function(){
+    //created : 시작했을 때(뷰 인스턴스 생성되었을 때)
+    //mounted : 시작했을 때(뷰 인스턴스 생성되어 마운트되었을 때)
+    //Vue js 생명 주기 검색하면 시작 시점 여러가지가 있다.
+
+    num = document.getElementsByClassName("con").length;
     alert(num);
 
-    for( let i=0; i<3; i++){
+    for( let i=0; i<num; i++){
       document.getElementsByClassName("con")[i].style.display="none";
     }
-    document.getElementsByClassName("con")[0].style.display="display";
+    document.getElementsByClassName("con")[0].style.display="block";
+  },
+
+  methods: {
+    AA:function(){
+      for( let i=0; i<num; i++){
+        document.getElementsByClassName("con")[i].style.display="none";
+      }
+      document.getElementsByClassName("con")[2].style.display="block";
+    },
   },
 }
 </script>
