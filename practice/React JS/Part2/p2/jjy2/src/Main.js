6a4634a0714993551a40928cac8dae0ffb760fc4
Jung jiyong <jjy9331@naver.com>
2022-05-13 10:44:11 +0900

    220513 react router p img add

--- practice/React JS/Part2/p2/jjy2/src/Main.js
+++ practice/React JS/Part2/p2/jjy2/src/Main.js
@@ -1,7 +1,17 @@
+import img1 from "./images/img1.jpg"
+import img2 from "./images/img2.jpg"
+import img3 from "./images/img3.jpg"
+import img4 from "./images/img4.jpg"
+
 function Main(){
     return  <div className='main'>
               <h2>Main</h2>
-              <p>메인입니다</p>
+              <div className="group">
+                  <div className="box"><img src={img1} alt="" /></div>
+                  <div className="box"><img src={img2} alt="" /></div>
+                  <div className="box"><img src={img3} alt="" /></div>
+                  <div className="box"><img src={img4} alt="" /></div>
+              </div>
             </div>
 }
 
