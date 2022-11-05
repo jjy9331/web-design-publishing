5866bb9a6c10e89226c4d60b0bbf1ec8cbfbdcc1
Jung jiyong <jjy9331@naver.com>
2022-11-06 02:30:23 +0900

    jjy_pf ver2-4 introduce scroll sequence ani canvas

--- portfolio/jjy_pf  test2-4/js/introduce.js
+++ portfolio/jjy_pf  test2-4/js/introduce.js
@@ -14,10 +14,15 @@ const preloadImages = () => {
   }
 };
 
+let W = $(window).width();
+let H = $(window).height();
+
 const img = new Image()
 img.src = currentFrame(1);
 canvas.width=1158;
 canvas.height=770;
+
+
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
