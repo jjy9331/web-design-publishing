4467a248b72609406d6c5830958236aa5049c56d
Jung jiyong <jjy9331@naver.com>
2023-04-28 00:35:28 +0900

    pf2-7 webpack test1 done

---
+++ portfolio/jjy_pf  test2-7/js/contact_ani.js
@@ -0,0 +1,130 @@
+
+const html2 = document.documentElement;
+const canvas2 = document.getElementById('screen2');
+const context2 = canvas2.getContext("2d");
+
+
+const canUseWebP2 = () => {
+    const elem = document.createElement('canvas');
+
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // was able or not to get WebP representation
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+
+    // very old browser like IE 8, canvas not supported
+    return false;
+};
+
+// WorkerPromise2(canUseWebP2());
+
+const folderName2 = canUseWebP2() ? 'wp_contact_ani3' : 'contact_ani3';
+const frameCount2 = 255;
+const img2 = Array.from({ length: frameCount2 }, () => null); // Create an array with frameCount number of null values
+
+const currentFrame2 = index2 => {
+    const fileExtension2 = canUseWebP2() ? 'webp' : 'png';
+    return `./${folderName2}/${index2.toString().padStart(3, '0')}.${fileExtension2}`;
+};
+
+
+const loadImage2 = (src) => {
+    return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.onload = () => {
+        resolve(image);
+    };
+    image.onerror = (err) => {
+        reject(err);
+    };
+    image.src = src;
+    });
+};
+    
+const preloadImages2 = async () => {
+    try {
+        const promises = img2.map((_, i2) => {
+            return loadImage2(currentFrame2(i2));
+        });
+        const images = await Promise.all(promises);
+            images.forEach((image, i) => {
+                img2[i] = image;
+            });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+
+// contact_ani img preload web worker
+
+const mine2 = {js:{type:'text/javascript'}};
+
+const WorkerPromise2 = (f) => {
+    return (data2) => {
+        return new Promise((resolve2, reject2) => {
+        const worker2 = new Worker(
+            URL.createObjectURL(
+                new Blob([`onmessage=e=>postMessage((${f})(e.data));`], mine2.js)
+            )
+        );
+            worker2.onmessage = (e) => resolve2(e.data2);
+            worker2.onerror = (e) => reject2(e.data2);
+            worker2.postMessage(data2);
+        });
+    };
+};
+
+
+const prewk2 = preloadImages2();
+WorkerPromise2(prewk2);
+
+
+img2.src = currentFrame2(1);
+
+canvas2.width = 1920
+canvas2.height = 1080
+canvas2.style.position = 'fixed';
+canvas2.style.bottom = '0%';
+canvas2.style.objectFit = 'cover';
+
+
+const updateImage2 = async index2 => {
+    const image = await loadImage2(currentFrame2(index2));
+    context2.drawImage(image, 0, 0);
+};
+
+
+
+window.addEventListener('scroll', () => {  
+    let scrollTop2 = html2.scrollTop;
+    let frameIndex2 = Math.round(((html2.scrollTop/250)-874.87));
+    
+
+
+    if (frameIndex2 == 0){
+        frameIndex2 = 0;
+    }
+    if (frameIndex2 >= 254){
+        frameIndex2 = 254;
+    }
+
+    // scroll ani start point 
+    if( scrollTop2 <= 218740){
+        frameIndex2 = 0;
+    } 
+    // console.log("frameIndex2: "+frameIndex2);
+    // console.log("scrollTop2: "+scrollTop2);
+    // console.log("frameCount2: "+frameCount2);
+    // console.log("scrollHeight: "+html2.scrollHeight);
+    // console.log("innerHeight2: "+window.innerHeight);
+
+
+    
+    requestAnimationFrame(() => updateImage2(frameIndex2))
+},{ passive: true });
+
+// preloadImages2();
+
+
---
+++ portfolio/jjy_pf  test2-7/js/lottie_logo_ani.js
@@ -0,0 +1,282 @@
+
+
+    // loading
+
+    // var pf_cr = document.querySelector("#container");
+
+    const lg_an = document.querySelector("#logo_ani");
+    const wrp = document.querySelector(".wrapper");
+    const prog = document.querySelector('#progress');
+    const progressBar = prog.querySelector('.progress-bar');
+    const progressText = prog.querySelector('.progress-text');
+    const imgLoad = imagesLoaded(document.body);
+    const imgTotal = imgLoad.images.length;
+    let imgLoaded = 0;
+    let current = 0;
+
+
+    const fadeIn = (element, duration, callback) => {
+        element.style.transition = `ease-in-out ${duration / 1000}s`;
+        element.style.opacity = 1;
+        
+        setTimeout(() => {
+            element.style.display = 'block';
+            if (typeof callback === 'function') callback();
+        }, duration);
+    }
+
+    const fadeOut = (element, duration, callback) => {
+        element.style.transition = `ease-in-out ${duration / 1000}s`;
+        element.style.opacity = 0;
+        
+        setTimeout(() => {
+            element.style.display = 'none';
+            if (typeof callback === 'function') callback();
+        }, duration);
+    }
+
+    lottie_logo = () => {
+        LottieInteractivity.create({
+            player:'#logo_ani',
+            renderer:'svg',
+            disableCheck:'true',
+            mode:"chain",
+            actions: [
+                {
+                    state: 'autoplay',
+                    delay: 1000
+                }
+            ]
+        });
+    }
+
+    
+
+    function isMobile(){
+        var UserAgent = navigator.userAgent;
+        if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+
+    const imgpg = () => {
+        imgLoad.on('progress', () => {
+            imgLoaded++;
+        });
+    };
+
+    const up_imgpg = () => {
+            let target = (imgLoaded / imgTotal) * 100;
+            current += (target - current) * 0.1;
+            progressBar.style.width = current + '%';
+            progressText.textContent = Math.floor(current) + '%';
+            // console.log("imgLoaded: "+imgLoaded)
+            // console.log("imgTotal: "+imgTotal)
+    }
+
+    const pg_fdo = () => {
+            prog.classList.add('progress-complete');
+            setTimeout(() => {
+                // progressBar.add(progressText).delay = 1000;
+                progressBar.style.opacity = 1;
+                progressText.style.opacity = 1;
+                fadeOut( prog, 300, () => {});
+            }, 1000);
+    } 
+
+    const lg_st = () => {
+            setTimeout(()=>{
+                lottie_logo();
+            },1000)
+    }
+
+    const lg_ed = () => {
+            fadeOut( lg_an, 300, () => {});
+    }
+
+    const cr_fdo = () => {
+            fadeOut(pf_cr, 300, () =>  {});
+            curs.style.display = "none";
+    } 
+
+    const stpp_fdi = () => {
+            fadeIn( stpp, 500,()=>{});
+    } 
+    const cr_fdi = () => {
+        setTimeout(()=>{
+            fadeIn( pf_cr, 300,()=>{});
+        },1000);
+    }
+
+    const pf_loading = () => {
+            lg_an.style.display = "block"; //로딩표시영역 사라짐
+            pf_b.style.position = "fixed";
+            pf_b.style.overflow = "hidden";
+            skr_by.style.backgroundColor = "black";
+            m_hr.style.display = "none";
+            sect.forEach(element => {
+                element.style.visibility = "hidden";
+            });
+            stpp.style.display = "block";
+            stpp.style.visibility = "visible";
+            stpp.style.opacity = 0;
+            wrp.style.display = "none";
+            curs.style.display = "none";
+            m_fr.style.display = "none";
+            pf_b.style.overflow = "hidden";
+    }
+
+    const pf_start = () => {
+            wrp.style.display = "block";
+            wrp.style.visibility = "visible";
+            sect.forEach(element => {
+                element.style.visibility = "visible";
+            });
+            pf_b.style.backgroundColor = "white";
+            pf_hb.style.overflow = "auto";
+            skr_by.style.backgroundColor = "white";
+            if(isMobile()){
+                curs.style.display = 'none' //모바일페이지
+            } else {
+                curs.style.display = 'block'  //PC용 페이지
+            }
+    } 
+
+
+    const mine3 = {js:{type:'text/javascript'}};
+
+    const WorkerPromise3 = (f) => {
+        let resolve3, reject3;
+        
+        const worker3 = Object.assign(
+            new Worker(
+                URL.createObjectURL(
+                new Blob([`onmessage=e=>postMessage((${f})(e.data3));`], mine3.js) 
+                )
+            ),
+            { onmessage3: (e) => resolve3(e.data3), onerror3: (e) => reject3(e.data3) } 
+        );
+        return (data3) =>
+            new Promise((res3, rej3) => {
+                resolve3 = res3;
+                reject3 = rej3;
+                worker3.postMessage(data3); 
+            });
+    };
+
+    
+
+    const WorkerPromise4 = (f) => {
+        let resolve4, reject4;
+        
+        const worker4 = Object.assign(
+            new Worker(
+                URL.createObjectURL(
+                    new Blob([`onmessage=e=>postMessage((${f})(e.data4));`], {type: 'application/javascript'})
+                )
+            ),
+            { onmessage4: (e) => resolve4(e.data4), onerror4: (e) => reject4(e.data4) } 
+        );
+        return (data4) =>
+            new Promise((res4, rej4) => {
+                resolve4 = res4;
+                reject4 = rej4;
+                worker4.postMessage(data4); 
+            });
+    };
+
+
+    // const pf_vid = ["../vid/main_1.mp4", "../vid/menu_1.mp4", "../vid/sub_1.mp4", "../vid/v_family_safety.mp4", "../vid/v_full_case_1.mp4", "../vid/v_main_1.mp4"];
+
+    // const ld_vid = () => {
+    //     window.addEventListener('load', () => {
+    //         const videoElements = document.querySelectorAll('video[preload="none"]');
+    //         videoElements.forEach((video) => {
+    //             pf_vid.forEach((src) => {
+    //                 const sourceElement = document.createElement('source');
+    //                 sourceElement.src = src;
+    //                 video.appendChild(sourceElement);
+    //             });
+    //             video.load();
+    //         });
+    //     });
+    // };
+    // WorkerPromise3(ld_vid);
+
+
+    const urls = ["../data/logo_intro.json", "../data/mohenic_intro.json", "../data/sound_wave.json", "../data/volvo_intro.json"];
+    
+    const lott_ld = () => {
+        Promise.all(
+            urls.map(url =>
+                fetch(url).then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Failed to fetch ${url}: ${response.status} ${response.statusText}`);
+                    }
+                        return response.json();
+                })
+            )
+        )
+        .then(dataArray => {
+            // do something with the array of JSON data
+        })
+        .catch(error => console.error(error));
+    }
+
+    WorkerPromise3(lott_ld());
+
+
+    // WorkerPromise3(imgpg());
+
+    const imagesProgress = () => {
+        return new Promise((resolve, reject) => {
+            // WorkerPromise3(imgpg);
+            imgpg();
+            const updateProgress = () => {
+                up_imgpg();
+                // WorkerPromise3(up_imgpg());
+                if (current >= 100) {
+                    clearInterval(progressTimer);
+                    pg_fdo();
+                    lg_st();
+                    cr_fdo();
+                    if (current === 100) {
+                        let t = 0;
+                        logoAni = () => {
+                            t = t + 1;
+                            if (t == 6) {
+                                lg_ed();
+                                stpp_fdi();
+                                cr_fdi();
+                                pf_start();
+                                clearInterval(t);
+                                resolve();
+                            } else if(t < 6){
+                                pf_loading();
+                            }     
+                        }
+                        setInterval(logoAni, 1000);
+                    }
+                }
+                if (current > 99.9) {
+                    current = 100;
+                }
+            }
+            // WorkerPromise3(updateProgress);
+            updateProgress()
+            const progressTimer = setInterval(updateProgress, 0);
+        });
+    };
+    // WorkerPromise3(imagesProgress);
+    imagesProgress();
+    
+    // (async () => {
+    //     await imagesProgress();
+    //     // Code to be executed after the images have finished loading
+    // })();
+
+
